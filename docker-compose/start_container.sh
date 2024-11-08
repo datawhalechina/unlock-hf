f2facd16a5ce9771dd29923121aa7d3e2c9a5ad8
moyanxinxu <wwxy.mail@gmail.com>
2024-11-08 15:43:35 +0000

    update Dockerfile

--- docker-compose/start_container.sh
+++ docker-compose/start_container.sh
@@ -2,4 +2,4 @@ docker run -d \
 --name linux \
 --mount source=/Users/moyanxinxu/Documents/unlock-hf,target=/root/Documents/unlock-hf,type=bind \
 -v huggingface:/root/.cache/huggingface/ \
--it moyanxinxu/self-ubuntu-myxx:v3.0 /bin/bash
+-it moyanxinxu/self-ubuntu-myxx:latest /bin/bash
